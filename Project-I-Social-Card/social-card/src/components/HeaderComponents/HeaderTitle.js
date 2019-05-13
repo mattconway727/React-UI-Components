5d83198432188ddf208bc5239b1da6c2e8c9d318
Matt Conway <mattconway727@gmail.com>
2019-05-13 19:00:57 -0400

    created HeaderTitle component

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,17 @@
-import React from 'react';
-import './Header.css';
+import React from "react";
+import "./Header.css";
+
+const HeaderTitle = () => {
+  return (
+    <div className="header-content">
+      <div className="header-title">
+        <h2>Lambda School</h2>
+        <p>@lambdaschool</p>
+        <p>•</p>
+        <p>26 jan</p>
+      </div>
+      <HeaderContent />
+    </div>
+  );
+};
+export default HeaderTitle;
