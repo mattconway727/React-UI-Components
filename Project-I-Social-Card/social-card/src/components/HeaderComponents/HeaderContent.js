b40118248ecb7916b4d94c32edca61c97ee82bf4
Matt Conway <mattconway727@gmail.com>
2019-05-14 21:32:20 -0400

    changed content in HeaderContent

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContent.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContent.js
@@ -4,11 +4,9 @@ import "./Header.css";
 const HeaderContent = () => {
   return (
     <p className="header-content">
-      Look at them little rascals. Without washing the brush, I'm gonna go right
-      into some Van Dyke Brown, some Burnt Umber, and a little bit of Sap Green.
-      The least little bit can do so much. By now you should be quite happy
-      about what's happening here. Almost everything is going to happen for you
-      automatically - you don't have to spend any time working or worrying.
+      Let's learn React by building simple interfaces with components. Don't try
+      to overthink it, just keep it simple and have fun. Once you feel
+      comfortable using components you are well on your way to mastering React!
     </p>
   );
 };
