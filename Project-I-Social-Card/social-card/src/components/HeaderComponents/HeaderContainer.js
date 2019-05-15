32dbbeba1388f4fc8a5d17b1c162618591c094af
Matt Conway <mattconway727@gmail.com>
2019-05-14 21:27:04 -0400

    created HeaderContent component

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./Header.css";
 
 const HeaderContainer = () => {
-  return <div className="header-container" />;
+  return <div className="header-container">{/* some stuff */}</div>;
 };
 
 export default HeaderContainer;
