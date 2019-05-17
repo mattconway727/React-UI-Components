e6a2c6525017665e48ad2e5d733f8a31e6ef41bf
Matt Conway <mattconway727@gmail.com>
2019-05-17 07:55:46 -0400

    Footer.js

---
+++ Project-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Footer.css";
+
+const Footer = () => {
+  return (
+    <div className="footer-container">
+      <i class="far fa-comment" />
+      <i class="fas fa-retweet" />
+      <i class="far fa-heart" />
+    </div>
+  );
+};
+
+export default Footer;
