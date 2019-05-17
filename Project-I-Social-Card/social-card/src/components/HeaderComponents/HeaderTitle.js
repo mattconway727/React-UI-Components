e4d42ea0f3aafb96d3934d464fe4064182c9d5c9
Matt Conway <mattconway727@gmail.com>
2019-05-17 19:38:46 -0400

    final changes

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,15 +1,15 @@
 import React from "react";
 import HeaderContent from "./HeaderContent";
 import "./Header.css";
-import moment from "moment";
-const time = moment();
+// import moment from "moment";
+// const time = moment();
 
 const HeaderTitle = () => {
   return (
     <div className="header-title-wrapper">
       <div className="header-title">
         Lambda School
-        <span className="time-stamp">{time.format("MMM Do ")}</span>
+        <span className="time-stamp">Jan 26</span>
         <HeaderContent />
       </div>
     </div>
@@ -17,3 +17,5 @@ const HeaderTitle = () => {
 };
 
 export default HeaderTitle;
+
+// {time.format("MMM Do ")}
