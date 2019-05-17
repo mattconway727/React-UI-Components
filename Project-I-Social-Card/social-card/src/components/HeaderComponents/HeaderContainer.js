72a5c9c1f8d1b28e0993042b754d23f72673d935
Matt Conway <mattconway727@gmail.com>
2019-05-17 19:19:05 -0400

    updated HeaderContainer.js

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -3,14 +3,12 @@ import "./Header.css";
 
 import ImageThumbnail from "./ImageThumbnail";
 import HeaderTitle from "./HeaderTitle";
-import HeaderContent from "./HeaderContent";
 
 const HeaderContainer = () => {
   return (
     <div className="header-container">
       <ImageThumbnail />
       <HeaderTitle />
-      <HeaderContent />
     </div>
   );
 };
