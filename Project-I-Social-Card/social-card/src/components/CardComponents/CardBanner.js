b42d67080799d1d5553a06af6e42d32e3bbd1a7f
Matt Conway <mattconway727@gmail.com>
2019-05-17 17:56:15 -0400

    updated CardBanner.js

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
@@ -3,12 +3,12 @@ import "./Card.css";
 
 const CardBanner = () => {
   return (
-    <div className="banner-container">
+    <a className="banner-container-wrapper" href="https://reactjs.org">
       <img
         className="banner-img"
         src="https://tk-assets.lambdaschool.com/fcd75197-7d12-46ec-bc9e-4130f34822fa_reactbackground.png"
       />
-    </div>
+    </a>
   );
 };
 
