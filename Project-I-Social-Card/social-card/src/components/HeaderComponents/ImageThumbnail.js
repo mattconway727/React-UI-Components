bddf04fba04cfe599486786eb147cc8ad86c0fd0
Matt Conway <mattconway727@gmail.com>
2019-05-17 19:23:12 -0400

    updated ImageThumbnail.js

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/ImageThumbnail.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/ImageThumbnail.js
@@ -3,8 +3,11 @@ import "./Header.css";
 
 const ImageThumbnail = () => {
   return (
-    <div className="image-thumbnail">
-      <img src="https://tk-assets.lambdaschool.com/1c1b7262-cf23-4a9f-90b6-da0d3c74a5c6_lambdacrest.png" />
+    <div className="thumb-wrapper">
+      <img
+        className="thumb-img"
+        src="https://tk-assets.lambdaschool.com/1c1b7262-cf23-4a9f-90b6-da0d3c74a5c6_lambdacrest.png"
+      />
     </div>
   );
 };
